a09cea2e8571246cead585fc27c37380fbfd2a3d
Coulibaly <coulwao@gmail.com>
2018-03-26 17:20:02 +0100

    ajout

--- app/helpers.php
+++ app/helpers.php
@@ -15,3 +15,14 @@ if(!function_exists('page_title'))
 		
 	}
 }
+
+
+
+if(!function_exists('set_active_route'))
+{
+	function set_active_route($route)
+	{
+		return Route::is($route) ? 'active' : '';
+		
+	}
+}
